5d5b32cb2a384bd7a828581324ae182992a31a17
Aleksandr Tochilin <alex8079@gmail.com>
2023-09-22 12:35:28 +0300

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
+
+export default defineConfig(() => {
+    return {
+        server: {
+            open: true,
+        },
+        build: {
+            outDir: 'build',
+        },
+        plugins: [
+            react(),
+            eslint(),
+            svgr({ svgrOptions: { icon: true } })
+        ],
+    };
+});
