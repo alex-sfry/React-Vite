2b3ef1e713ed67db9d338ec265dd7521b7cf0ea1
Aleksandr Tochilin <alex8079@gmail.com>
2023-09-29 18:18:50 +0300

    add @vitejs/plugin-legacy, @babel/preset-env

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig(() => {
     return {
@@ -14,7 +15,10 @@ export default defineConfig(() => {
         plugins: [
             react(),
             eslint(),
-            svgr({ svgrOptions: { icon: true } })
+            svgr({ svgrOptions: { icon: true } }),
+            legacy({
+                targets: ['last 2 versions and not dead, > 0.2%, Firefox ESR'],
+            })
         ],
     };
 });
