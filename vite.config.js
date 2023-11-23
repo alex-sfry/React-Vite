b9ab8faf2f384cf8ebdd5afff3d9da7635737fe6
Aleksandr Tochilin <alex8079@gmail.com>
2023-11-23 14:35:38 +0300

    update vite to 5.x, major changes in eslint

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// eslint-disable-next-line import/namespace
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import eslint from 'vite-plugin-eslint';
 import svgr from 'vite-plugin-svgr';
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig(() => {
     return {
@@ -16,9 +16,6 @@ export default defineConfig(() => {
             react(),
             eslint(),
             svgr({ svgrOptions: { icon: true } }),
-            legacy({
-                targets: ['last 2 versions and not dead, > 0.2%, Firefox ESR'],
-            })
         ],
     };
 });
